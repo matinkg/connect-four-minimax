3017755a2ba751fd07d1dae5621f50c31442e493
Axone <draxone13@gmail.com>
2024-06-21 22:28:15 +0330

    Add: heuristic function

--- main.py
+++ main.py
@@ -27,31 +27,9 @@ class Board:
                 self.board[row][column] = player
                 break
 
-class ConnectFour:
-    def __init__(self):
-        self.board = Board()
-        self.gameOver = False
-        self.turn = PLAYER1
-        self.winner = None
+    def checkWin(self):
+        board = self.board
 
-    def makeMove(self):
-        print(f"Player {self.turn}'s turn")
-        column = input("Enter column number: ")
-
-        # check if column is not integer or out of range
-        if not column.isdigit() or int(column) < 1 or int(column) > 7:
-            print("Invalid input. Please enter a number between 1 and 7...")
-            return self.makeMove()
-        column = int(column)
-
-        self.board.dropPiece(column - 1, self.turn)
-
-    def switchTurn(self):
-        self.turn = PLAYER1 if self.turn == PLAYER2 else PLAYER2
-
-    @staticmethod
-    def checkWin(board):
-        board = board.board
         # check horizontal
         for row in range(6):
             for col in range(4):
@@ -76,7 +54,91 @@ class ConnectFour:
                     return board[row][col]
 
         return None
+
+    # a function to evaluate the window
+    # a window is a list of 4 cells in the board (row, column, diagonal)
+    @staticmethod
+    def evaluate_window(window, player):
+        score = 0
+        opponent = PLAYER1 if player == PLAYER2 else PLAYER2
+
+        if window.count(player) == 4:
+            score += 100
+        elif window.count(player) == 3 and window.count(EMPTY) == 1:
+            score += 5
+        elif window.count(player) == 2 and window.count(EMPTY) == 2:
+            score += 2
+
+        if window.count(opponent) == 3 and window.count(EMPTY) == 1:
+            score -= 4
+
+        return score
+
+    # a function to evaluate user score based on the board state
+    # it is a simple heuristic function to evaluate the score of the board
+    # used by the AI to make a move (minimax algorithm)
+    def evalScore(self, player):
+        score = 0
         
+        # the center column is the best column to play in because it gives the player more options
+        # so we give it a little higher score
+        center_column = []
+        for row in range(6):
+            center_column.append(self.board[row][3])
+        center_count = center_column.count(player)
+        score += center_count * 3
+
+        # horizontal
+        for row in range(6):
+            row_array = self.board[row]
+            for col in range(4):
+                window = row_array[col:col + 4]
+                score += self.evaluate_window(window, player)
+
+        # vertical
+        for col in range(7):
+            col_array = []
+            for row in range(6):
+                col_array.append(self.board[row][col])
+                
+            for row in range(3):
+                window = col_array[row:row + 4]
+                score += self.evaluate_window(window, player)
+
+        # diagonal
+        for row in range(3):
+            for col in range(4):
+                window = [self.board[row + i][col + i] for i in range(4)]
+                score += self.evaluate_window(window, player)
+        
+        for row in range(3):
+            for col in range(4):
+                window = [self.board[row + 3 - i][col + i] for i in range(4)]
+                score += self.evaluate_window(window, player)
+        
+        return score
+
+class ConnectFour:
+    def __init__(self):
+        self.board = Board()
+        self.gameOver = False
+        self.turn = PLAYER1
+        self.winner = None
+
+    def makeMove(self):
+        print(f"Player {self.turn}'s turn")
+        column = input("Enter column number: ")
+
+        # check if column is not integer or out of range
+        if not column.isdigit() or int(column) < 1 or int(column) > 7:
+            print("Invalid input. Please enter a number between 1 and 7...")
+            return self.makeMove()
+        column = int(column)
+
+        self.board.dropPiece(column - 1, self.turn)
+
+    def switchTurn(self):
+        self.turn = PLAYER1 if self.turn == PLAYER2 else PLAYER2
 
     def playGame(self):
         # random turn
@@ -89,7 +151,7 @@ class ConnectFour:
             self.makeMove()
             self.board.printBoard()
             
-            if self.checkWin(self.board):
+            if self.board.checkWin():
                 self.gameOver = True
                 self.winner = self.turn
                 print(f"Player {self.winner} wins!")
